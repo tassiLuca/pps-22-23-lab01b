34593484bea8da2253a6691ff84bd80f384f70b0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-10 00:30:22 +0100

    refactor: remove unused throws decl

--- src/e1/Main.java
+++ src/e1/Main.java
@@ -1,7 +1,9 @@
 package e1;
 
+import e1.view.GUI;
+
 public class Main {
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args) {
         new GUI();
     }
 }
