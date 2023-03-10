5bd3ca57b298fbb78c75af3e96ab3068e7ae4210
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-10 08:12:49 +0100

    Update javadoc

--- src/e1/model/MovableEntity.java
+++ src/e1/model/MovableEntity.java
@@ -2,12 +2,15 @@ package e1.model;
 
 import utils.Position;
 
+/**
+ * An interface modeling a movable entty, i.e. an entity that can move.
+ */
 public interface MovableEntity extends StaticEntity {
 
     /**
      * Try to attempt a movement, updating the position of the entity
-     * only if the given new position is allowed.
-     * @param newPosition the {@link Position} where to move the entity
+     * only if the given new one is allowed.
+     * @param newPosition the {@link Position} where to move the entity.
      */
     boolean moveTo(Position newPosition);
 }
