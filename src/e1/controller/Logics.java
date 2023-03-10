faef136e3696efdebe95cfa3e23382d46ad0747f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-10 07:47:42 +0100

    Add doc

--- src/e1/controller/Logics.java
+++ src/e1/controller/Logics.java
@@ -6,11 +6,26 @@ import java.util.Set;
 
 public interface Logics {
 
+    /**
+     * Attempts to move the knight in the given position.
+     * if the movement is not valid the operation is not performed.
+     * @param newPosition the position where move the knight.
+     */
     void hit(Position newPosition);
 
+    /**
+     * @return true if the knight ate all the pawns in the grid, 
+     * i.e. no pawns are left, false otherwise.
+     */
     boolean isOver();
 
+    /**
+     * @return a {@link Position} describing the knight position inside the board.
+     */
     Position getKnightPosition();
 
+    /**
+     * @return a {@link Set} containing all the pawns positions inside the board.
+     */
     Set<Position> getPawnsPosition();
 }
