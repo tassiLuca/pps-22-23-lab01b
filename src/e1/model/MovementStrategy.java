a4a3733f57bea811a880b0f30cf3dbe8158d1350
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-10 08:08:06 +0100

    Fix javadoc

--- src/e1/model/MovementStrategy.java
+++ src/e1/model/MovementStrategy.java
@@ -3,13 +3,13 @@ package e1.model;
 import utils.Position;
 
 /**
- * A strategy encapsulating the movement logic of an {@link StaticEntity}.
+ * A strategy encapsulating the movement logic of a {@link MovableEntity}.
  */
 interface MovementStrategy {
 
     /**
-     * @param actualPosition the current position of the entity
-     * @param newPosition the desired next position of the entity
+     * @param actualPosition the current position of the entity.
+     * @param newPosition the desired next position of the entity.
      * @return true if the move is feasible, false otherwise.
      */
     boolean isMovementFeasible(Position actualPosition, Position newPosition);
