2d19b94751a0604b88da17731876fb9818e3ec4e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-10 08:35:15 +0100

    Add javadoc

--- src/e1/model/Grid.java
+++ src/e1/model/Grid.java
@@ -10,9 +10,19 @@ import java.util.Set;
  */
 public interface Grid {
 
+    /**
+     * @returm a {@link Set} containing the pawns {@link Position}s.
+     */
     Set<Position> getPawnsPosition();
 
+    /**
+     * @return the knight {@link Position}.
+     */
     Position getKnightPosition();
 
+    /**
+     * Attempts to move the knight in the given position.
+     * @param newPosition the position where to move the knight.
+     */
     void moveKnight(final Position newPosition);
 }
